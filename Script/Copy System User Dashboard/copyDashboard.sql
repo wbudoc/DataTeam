f27262579e5cbf5c1127a0efe03aa5f4ec80c7ac
wbudoc <wbu@dev.neoncrm.com>
2020-09-01 14:50:05 +0800

    git配置

--- Script/Copy System User Dashboard/copyDashboard.sql
+++ Script/Copy System User Dashboard/copyDashboard.sql
@@ -1,123 +1,109 @@
 
 ##########################################################################################################################
-#                            copy one system user dashboard to another system user
-#                                                  
-#                                                  CC-3459
+#                            copy system user dashboard to other system user
+#
+#                                                  CC-3459,CC-2735,CC-3059
 #
 ##########################################################################################################################
-DROP TABLE if EXISTS 
+DROP TABLE IF EXISTS
 z_newcreate_dsh_dashboard_copy_20200512,
 z_newcreate_dsh_dashboard_delete_20200512,
+z_newcreate_dsh_dashboard_insert_20200512,
 z_newcreate_dsh_dashboard_widget_copy_20200512,
 z_newcreate_dsh_dashboard_widget_delete_20200512,
+z_newcreate_dsh_dashboard_widget_insert_20200512,
 z_newcreate_dsh_dashboard_widget_output_copy_20200512,
 z_newcreate_dsh_dashboard_widget_output_delete_20200512,
+z_newcreate_dsh_dashboard_widget_output_insert_20200512,
 z_newcreate_dsh_dashboard_widget_search_criteria_copy_20200512,
-z_newcreate_dsh_dashboard_widget_search_criteria_delete_20200512;
+z_newcreate_dsh_dashboard_widget_search_criteria_delete_20200512,
+z_newcreate_dsh_dashboard_widget_search_criteria_insert_20200512;
 
 ##########################################################################################################################
 #copy fundraising dashboard(copy system user)
 #type: Mission Control(1), ACCOUNT(2), FUNDRAISING (3), MEMBERSHIP (4), EVENT(5), REPORT(6), Email(7), STORE(8), DOCUMENT(9) 最后一个type=9 的还没有这个功能
 ##########################################################################################################################
 CREATE TABLE z_newcreate_dsh_dashboard_copy_20200512
-SELECT * from dsh_dashboard where userId in(3) and type=3;
+SELECT * FROM dsh_dashboard WHERE userId IN(4) AND type IN(1,2);
 
 CREATE TABLE z_newcreate_dsh_dashboard_widget_copy_20200512
-SELECT * from dsh_dashboard_widget where dashboardId in (SELECT id from z_newcreate_dsh_dashboard_copy_20200512);
+SELECT dd.* FROM dsh_dashboard_widget dd JOIN z_newcreate_dsh_dashboard_copy_20200512 zn ON dd.dashboardId=zn.id;
 
 CREATE TABLE z_newcreate_dsh_dashboard_widget_output_copy_20200512
-SELECT * from dsh_dashboard_widget_output WHERE dashboardWidgetId in (SELECT id from z_newcreate_dsh_dashboard_widget_copy_20200512);
+SELECT dd.* FROM dsh_dashboard_widget_output dd JOIN z_newcreate_dsh_dashboard_widget_copy_20200512 zn ON dd.dashboardWidgetId=zn.id;
 
 CREATE TABLE z_newcreate_dsh_dashboard_widget_search_criteria_copy_20200512
-SELECT * from dsh_dashboard_widget_search_criteria where dashboardWidgetId in (SELECT id from z_newcreate_dsh_dashboard_widget_copy_20200512);
+SELECT dd.* FROM dsh_dashboard_widget_search_criteria dd JOIN z_newcreate_dsh_dashboard_widget_copy_20200512 zn ON dd.dashboardWidgetId=zn.id;
 
 ##########################################################################################################################
-#delete dafault fundraising dashboard(aother system user)
+#delete dafault fundraising dashboard(another system user)
 ##########################################################################################################################
 CREATE TABLE z_newcreate_dsh_dashboard_delete_20200512
-SELECT * from dsh_dashboard where userId in(9811) and type=3;
+SELECT * FROM dsh_dashboard WHERE userId IN(1,3) AND type IN(1,2);
 
 CREATE TABLE z_newcreate_dsh_dashboard_widget_delete_20200512
-SELECT * from dsh_dashboard_widget where dashboardId in (SELECT id from z_newcreate_dsh_dashboard_delete_20200512);
+SELECT dd.* FROM dsh_dashboard_widget dd JOIN z_newcreate_dsh_dashboard_delete_20200512 zn WHERE dd.dashboardId=zn.id;
 
 CREATE TABLE z_newcreate_dsh_dashboard_widget_output_delete_20200512
-SELECT * from dsh_dashboard_widget_output WHERE dashboardWidgetId in (SELECT id from z_newcreate_dsh_dashboard_widget_delete_20200512);
+SELECT dd.* FROM dsh_dashboard_widget_output dd JOIN z_newcreate_dsh_dashboard_widget_delete_20200512 zn WHERE dd.dashboardWidgetId=zn.id;
 
 CREATE TABLE z_newcreate_dsh_dashboard_widget_search_criteria_delete_20200512
-SELECT * from dsh_dashboard_widget_search_criteria where dashboardWidgetId in (SELECT id from z_newcreate_dsh_dashboard_widget_delete_20200512);
+SELECT dd.* FROM dsh_dashboard_widget_search_criteria dd JOIN z_newcreate_dsh_dashboard_widget_delete_20200512 zn WHERE dd.dashboardWidgetId=zn.id;
 
-DELETE from dsh_dashboard_widget_search_criteria where id in (SELECT id from z_newcreate_dsh_dashboard_widget_search_criteria_delete_20200512);
-DELETE from dsh_dashboard_widget_output where id in (SELECT id from z_newcreate_dsh_dashboard_widget_output_delete_20200512);
-DELETE from dsh_dashboard_widget where id in (SELECT id from z_newcreate_dsh_dashboard_widget_delete_20200512);
-DELETE from dsh_dashboard where id in (SELECT id from z_newcreate_dsh_dashboard_delete_20200512);
+DELETE dd.* FROM dsh_dashboard_widget_search_criteria dd JOIN z_newcreate_dsh_dashboard_widget_search_criteria_delete_20200512 zn ON dd.id=zn.id;
+DELETE dd.* FROM dsh_dashboard_widget_output dd JOIN z_newcreate_dsh_dashboard_widget_output_delete_20200512 zn ON dd.id=zn.id;
+DELETE dd.* FROM dsh_dashboard_widget dd JOIN z_newcreate_dsh_dashboard_widget_delete_20200512 zn ON dd.id=zn.id;
+DELETE dd.* FROM dsh_dashboard dd JOIN z_newcreate_dsh_dashboard_delete_20200512 zn ON dd.id=zn.id;
 
 ##########################################################################################################################
 #insert copy fundraising dashboard
 ##########################################################################################################################
-UPDATE z_newcreate_dsh_dashboard_copy_20200512 set userId=9811;
+CREATE TABLE z_newcreate_dsh_dashboard_insert_20200512
+SELECT zn.id,u.userId,zn.type,zn.defaultDashboard,zn.id oldId FROM z_newcreate_dsh_dashboard_copy_20200512 zn JOIN (SELECT userId FROM user WHERE userId IN(1,3)) u ORDER BY u.userId,zn.id;
 
-ALTER TABLE z_newcreate_dsh_dashboard_copy_20200512 add oldId int DEFAULT null;
+SELECT IFNULL(max(id+0),0) FROM dsh_dashboard INTO @dshId;
+UPDATE z_newcreate_dsh_dashboard_insert_20200512 SET id=(@dshId:=@dshId+1);
 
-UPDATE z_newcreate_dsh_dashboard_copy_20200512 set oldId=id;
-
-SELECT IFNULL(max(id+0),0) from dsh_dashboard into @dshId;
-
-UPDATE z_newcreate_dsh_dashboard_copy_20200512 set id=(@dshId:=@dshId+1);
-
-insert INTO dsh_dashboard(id,userId,type,defaultDashboard)
-SELECT id,userId,type,defaultDashboard from z_newcreate_dsh_dashboard_copy_20200512;
+INSERT INTO dsh_dashboard(id,userId,type,defaultDashboard)
+SELECT id,userId,type,defaultDashboard FROM z_newcreate_dsh_dashboard_insert_20200512;
 
 ##########################################################################################################################
 #import dsh_dashboard_widget
 ##########################################################################################################################
+CREATE TABLE z_newcreate_dsh_dashboard_widget_insert_20200512
+SELECT zn.id,zn.title,zn2.Id dashboardId,zn.systemWidgetId,zn.`columns`,zn.pageSize,zn.displayOrder,zn.enablePosting,zn.sortField,zn.sortDirection,zn.id oldId FROM z_newcreate_dsh_dashboard_widget_copy_20200512 zn JOIN z_newcreate_dsh_dashboard_insert_20200512 zn2 ON zn.dashboardId=zn2.oldId ORDER BY zn2.Id;
 
-UPDATE z_newcreate_dsh_dashboard_widget_copy_20200512 zjd, z_newcreate_dsh_dashboard_copy_20200512 zj set zjd.dashboardId=zj.id where zjd.dashboardId=zj.oldId;
+SELECT IFNULL(max(id+0),0) FROM dsh_dashboard_widget INTO @dshId;
+UPDATE z_newcreate_dsh_dashboard_widget_insert_20200512 SET id=(@dshId:=@dshId+1);
 
-ALTER TABLE z_newcreate_dsh_dashboard_widget_copy_20200512 add oldId int DEFAULT null;
-
-UPDATE z_newcreate_dsh_dashboard_widget_copy_20200512 set oldId=id;
-
-SELECT IFNULL(max(id+0),0) from dsh_dashboard_widget into @dshId;
-
-UPDATE z_newcreate_dsh_dashboard_widget_copy_20200512 set id=(@dshId:=@dshId+1);
-
-insert into dsh_dashboard_widget (id,title,dashboardId,systemWidgetId,columns,pageSize,displayOrder,enablePosting,sortField,sortDirection)
-SELECT id,title,dashboardId,systemWidgetId,columns,pageSize,displayOrder,enablePosting,sortField,sortDirection from z_newcreate_dsh_dashboard_widget_copy_20200512;
-
-##########################################################################################################################
-UPDATE z_newcreate_dsh_dashboard_widget_output_copy_20200512 zjd, z_newcreate_dsh_dashboard_widget_copy_20200512 zj set zjd.dashboardWidgetId=zj.id where zjd.dashboardWidgetId=zj.oldId;
-UPDATE z_newcreate_dsh_dashboard_widget_search_criteria_copy_20200512 zjd, z_newcreate_dsh_dashboard_widget_copy_20200512 zj set zjd.dashboardWidgetId=zj.id where zjd.dashboardWidgetId=zj.oldId;
+INSERT INTO dsh_dashboard_widget (id,title,dashboardId,systemWidgetId,`columns`,pageSize,displayOrder,enablePosting,sortField,sortDirection)
+SELECT id,title,dashboardId,systemWidgetId,`columns`,pageSize,displayOrder,enablePosting,sortField,sortDirection from z_newcreate_dsh_dashboard_widget_insert_20200512;
 
 ##########################################################################################################################
 #import dsh_dashboard_widget_output
 ##########################################################################################################################
+CREATE TABLE z_newcreate_dsh_dashboard_widget_output_insert_20200512
+SELECT zn.id,zn2.Id dashboardWidgetId,zn.systemOutputId,zn.displayOrder,zn.selected,zn.id oldId FROM z_newcreate_dsh_dashboard_widget_output_copy_20200512 zn JOIN z_newcreate_dsh_dashboard_widget_insert_20200512 zn2 ON zn.dashboardWidgetId=zn2.oldId ORDER BY zn2.Id;
 
-ALTER TABLE z_newcreate_dsh_dashboard_widget_output_copy_20200512 add oldId int DEFAULT null;
+SELECT IFNULL(max(id+0),0) FROM dsh_dashboard_widget_output INTO @dshId;
+UPDATE z_newcreate_dsh_dashboard_widget_output_insert_20200512 SET id=(@dshId:=@dshId+1);
 
-UPDATE z_newcreate_dsh_dashboard_widget_output_copy_20200512 set oldId=id;
-
-SELECT IFNULL(max(id+0),0) from dsh_dashboard_widget_output into @dshId;
-
-UPDATE z_newcreate_dsh_dashboard_widget_output_copy_20200512 set id=(@dshId:=@dshId+1);
-
-insert into dsh_dashboard_widget_output (id,dashboardWidgetId,systemOutputId,displayOrder,selected)
-SELECT id,dashboardWidgetId,systemOutputId,displayOrder,selected from z_newcreate_dsh_dashboard_widget_output_copy_20200512;
+INSERT INTO dsh_dashboard_widget_output (id,dashboardWidgetId,systemOutputId,displayOrder,selected)
+SELECT id,dashboardWidgetId,systemOutputId,displayOrder,selected FROM z_newcreate_dsh_dashboard_widget_output_insert_20200512;
 
 ##########################################################################################################################
 #import dsh_dashboard_widget_search_criteria
 ##########################################################################################################################
-ALTER TABLE z_newcreate_dsh_dashboard_widget_search_criteria_copy_20200512 add oldId int DEFAULT null;
+CREATE TABLE z_newcreate_dsh_dashboard_widget_search_criteria_insert_20200512
+SELECT zn.id,zn2.Id dashboardWidgetId,zn.systemCriteriaId,zn.`value`,zn.`enable`,zn.id oldId FROM z_newcreate_dsh_dashboard_widget_search_criteria_copy_20200512 zn JOIN z_newcreate_dsh_dashboard_widget_insert_20200512 zn2 ON zn.dashboardWidgetId=zn2.oldId ORDER BY zn2.Id;
 
-UPDATE z_newcreate_dsh_dashboard_widget_search_criteria_copy_20200512 set oldId=id;
+SELECT IFNULL(max(id+0),0) FROM dsh_dashboard_widget_search_criteria INTO @dshId;
+UPDATE z_newcreate_dsh_dashboard_widget_search_criteria_insert_20200512 SET id=(@dshId:=@dshId+1);
 
-SELECT IFNULL(max(id+0),0) from dsh_dashboard_widget_search_criteria into @dshId;
-
-UPDATE z_newcreate_dsh_dashboard_widget_search_criteria_copy_20200512 set id=(@dshId:=@dshId+1);
-
-insert into dsh_dashboard_widget_search_criteria (id,dashboardWidgetId,systemCriteriaId,value,enable)
-SELECT id,dashboardWidgetId,systemCriteriaId,value,enable from z_newcreate_dsh_dashboard_widget_search_criteria_copy_20200512;
+INSERT INTO dsh_dashboard_widget_search_criteria (id,dashboardWidgetId,systemCriteriaId,`value`,`enable`)
+SELECT id,dashboardWidgetId,systemCriteriaId,`value`,`enable` from z_newcreate_dsh_dashboard_widget_search_criteria_insert_20200512;
 
 ##########################################################################################################################
-update organization_bio set calculateFiscalStats = 1;
+UPDATE organization_bio SET calculateFiscalStats = 1;
 
-commit;
+COMMIT;
